33cfd0d815ab81ac6db2ddeb601a68a8d484ed89
Nishant Sharma <nishant.graviton@outlook.com>
2023-10-02 20:44:16 +0530

    applying data transformation and ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,8 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -37,7 +39,7 @@ class DataIngestion:
 
             logging.info("Train test split initiated")
 
-            train_set,test_set = train_test_split(df,train_size=0.8, random_state=12)
+            train_set,test_set = train_test_split(df,test_size=0.2, random_state=12)
 
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
 
@@ -58,6 +60,11 @@ class DataIngestion:
 if __name__ == "__main__":
 
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
+
+
 
 
